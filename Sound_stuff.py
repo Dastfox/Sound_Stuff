875d0175314dd6fcd89978accd7288ba82ace7c0
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-06-22 17:52:50 +0200

    multiple files supported

--- Sound_stuff.py
+++ Sound_stuff.py
@@ -6,7 +6,7 @@ import matplotlib.pylab as plt
 import seaborn as sns
 from os import listdir
 from os.path import isfile, join
-
+from pathlib import Path
 from glob import glob
 
 import librosa as lr
@@ -21,27 +21,28 @@ color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 color_cycle = cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
 
 
-# setting music's base dir
+# setting music's base dir & file names dict
 data_dir = './SoundFiles'
 audio_files = glob(f'{data_dir}/*.wav')
 file_names = [f for f in listdir('SoundFiles') if isfile(join('SoundFiles', f))]
 
-# loading the music (1st)
-ipd.Audio(audio_files[0])
-y, sr = librosa.load(audio_files[0])
+# loading the music (1st) and saving the png
+
+for i in range(len(audio_files)):
+    name = f'{Path(audio_files[i]).stem}'
+    ipd.Audio(audio_files[i])
+    y, sr = lr.load(audio_files[i])
+    y_trimmed, _ = librosa.effects.trim(y, top_db=20)
+    pd.Series(y_trimmed).plot(figsize=(20, 5),
+                  lw=1,
+                  title=name,
+                 color=color_pal[1])
+    plt.savefig("./Plots/"f'{name}_WF.png')
 
 # to uncomment if seeing plot data is needed
 # print(f'y: {y[:10]}')
 # print(f'shape y: {y.shape}')
 # print(f'sr: {sr}')
-
-
-
-pd.Series(y).plot(figsize=(10, 5),
-                  lw=1,
-                  title='Waveform',
-                 color=color_pal[0])
-plt.savefig("./Plots/"f'{file_names[0]}_WF.png')
 
 
 
