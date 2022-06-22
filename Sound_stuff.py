971f6770522e79949f34e929585faac7eefa4cd9
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-06-22 21:00:01 +0200

    name management with tempo integration

--- Sound_stuff.py
+++ Sound_stuff.py
@@ -24,20 +24,22 @@ color_cycle = cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
 # setting music's base dir & file names dict
 data_dir = './SoundFiles'
 audio_files = glob(f'{data_dir}/*.wav')
-file_names = [f for f in listdir('SoundFiles') if isfile(join('SoundFiles', f))]
 
-# loading the music (1st) and saving the png
+# loading the music and saving the png
 
 for i in range(len(audio_files)):
-    name = f'{Path(audio_files[i]).stem}'
+    
     ipd.Audio(audio_files[i])
     y, sr = lr.load(audio_files[i])
+    tempo = librosa.beat.tempo(y=y, sr=sr)
+    tempoR = '%G' % (tempo,)
+    name = f'{Path(audio_files[i]).stem} {tempoR} bpm'
     y_trimmed, _ = librosa.effects.trim(y, top_db=20)
     pd.Series(y_trimmed).plot(figsize=(20, 5),
                   lw=1,
                   title=name,
                  color=color_pal[1])
-    plt.savefig("./Plots/"f'{name}_WF.png')
+    plt.savefig("./Plots/"f'{name} WF.png')
 
 # to uncomment if seeing plot data is needed
 # print(f'y: {y[:10]}')
